972db4ee15ca7736d6af3af0a29cc599cd49767f
lvsiyu <1030502996@qq.com>
2021-11-18 13:55:33 +0800

    feat: 新增表情包库

---
+++ src/utils/emoticons.ts
@@ -0,0 +1,91 @@
+import angle from '@/asserts/svg/angle.svg';
+import angry from '@/asserts/svg/angry.svg';
+import confused from '@/asserts/svg/confused.svg';
+import cool from '@/asserts/svg/cool.svg';
+import cool2 from '@/asserts/svg/cool2.svg';
+import cry1 from '@/asserts/svg/cry1.svg';
+import cry2 from '@/asserts/svg/cry2.svg';
+import devil from '@/asserts/svg/devil.svg';
+import dizziness from '@/asserts/svg/dizziness.svg';
+import exhausted from '@/asserts/svg/exhausted.svg';
+import face_mask from '@/asserts/svg/face_mask.svg';
+import happy1 from '@/asserts/svg/happy1.svg';
+import happy2 from '@/asserts/svg/happy2.svg';
+import happy3 from '@/asserts/svg/happy3.svg';
+import injured from '@/asserts/svg/injured.svg';
+import laugh1 from '@/asserts/svg/laugh1.svg';
+import laugh2 from '@/asserts/svg/laugh2.svg';
+import laugh3 from '@/asserts/svg/laugh3.svg';
+import mute from '@/asserts/svg/mute.svg';
+import poisoning1 from '@/asserts/svg/poisoning1.svg';
+import poisoning2 from '@/asserts/svg/poisoning2.svg';
+import poker_face1 from '@/asserts/svg/poker_face1.svg';
+import poker_face2 from '@/asserts/svg/poker_face2.svg';
+import sad1 from '@/asserts/svg/sad1.svg';
+import sad2 from '@/asserts/svg/sad2.svg';
+import scared1 from '@/asserts/svg/scared1.svg';
+import scared2 from '@/asserts/svg/scared2.svg';
+import shock1 from '@/asserts/svg/shock1.svg';
+import shock2 from '@/asserts/svg/shock2.svg';
+import shut_up from '@/asserts/svg/shut_up.svg';
+import sick from '@/asserts/svg/sick.svg';
+import sinister_smile from '@/asserts/svg/sinister_smile.svg';
+import sleep from '@/asserts/svg/sleep.svg';
+import smile from '@/asserts/svg/smile.svg';
+import squint from '@/asserts/svg/squint.svg';
+import stupid from '@/asserts/svg/stupid.svg';
+import sweat from '@/asserts/svg/sweat.svg';
+import think from '@/asserts/svg/think.svg';
+import tongue1 from '@/asserts/svg/tongue1.svg';
+import tongue2 from '@/asserts/svg/tongue2.svg';
+import tongue3 from '@/asserts/svg/tongue3.svg';
+import vomiting from '@/asserts/svg/vomiting.svg';
+import wink from '@/asserts/svg/wink.svg';
+
+const randomLogo = [
+  angle,
+  angry,
+  confused,
+  cool,
+  cool2,
+  cry1,
+  cry2,
+  devil,
+  dizziness,
+  exhausted,
+  face_mask,
+  happy1,
+  happy2,
+  happy3,
+  injured,
+  laugh1,
+  laugh2,
+  laugh3,
+  mute,
+  poisoning1,
+  poisoning2,
+  poker_face1,
+  poker_face2,
+  sad1,
+  sad2,
+  scared1,
+  scared2,
+  shock1,
+  shock2,
+  shut_up,
+  sick,
+  sinister_smile,
+  sleep,
+  smile,
+  squint,
+  stupid,
+  sweat,
+  think,
+  tongue1,
+  tongue2,
+  tongue3,
+  vomiting,
+  wink,
+];
+
+export { randomLogo };
