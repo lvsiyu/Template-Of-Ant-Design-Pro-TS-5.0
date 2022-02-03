e825986fe335558f6cf231bbc3dc8c3bed0e3bbf
lvsiyu <lvsiyu@lvsiyudeMacBook-Pro.local>
2022-02-03 17:00:35 +0800

    feat: 新增列表表单提交验证

---
+++ src/pages/Form/ListForm/data/index.d.ts
@@ -0,0 +1,9 @@
+export type basisFormDataType = {
+  text1?: string;
+};
+
+export type basisFormRespionse = {
+  code: number;
+  data: basisFormDataType;
+  msg: string;
+};
--- src/pages/Form/ListForm/index.tsx
+++ src/pages/Form/ListForm/index.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import { message } from 'antd';
 import { PageContainer, FooterToolbar } from '@ant-design/pro-layout';
 import ProCard from '@ant-design/pro-card';
+import { uploadBasisForm } from './services';
 import ProForm, { ProFormList, ProFormText, ProFormDependency } from '@ant-design/pro-form';
+import type { basisFormDataType } from './data';
 
 const BasisForm: React.FC = () => {
+  const submitForm = (values: basisFormDataType) => {
+    uploadBasisForm(values).then((data) => {
+      if (data.code === 200) {
+        message.success('提交成功');
+      } else {
+        message.error('提交失败');
+      }
+    });
+  };
+
   return (
     <PageContainer>
       <ProCard>
         <ProForm
           submitter={{
             render: (_, dom) => <FooterToolbar>{dom}</FooterToolbar>,
+          }}
+          onFinish={async (values) => {
+            submitForm(values);
           }}
         >
           <ProFormList
