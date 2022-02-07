bdf9997e1014aa314746375b5df01f21aa8b85e8
lvsiyu <1030502996@qq.com>
2022-02-07 10:15:56 +0800

    feat: 新增嵌套列表表单

--- src/pages/Form/ListForm/index.tsx
+++ src/pages/Form/ListForm/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { message } from 'antd';
-import { PageContainer, FooterToolbar } from '@ant-design/pro-layout';
+import { PageContainer } from '@ant-design/pro-layout';
 import ProCard from '@ant-design/pro-card';
 import { uploadListForm } from './services';
-import ProForm, { ProFormList, ProFormText, ProFormDependency } from '@ant-design/pro-form';
+import ProForm, {
+  ProFormList,
+  ProFormText,
+  ProFormDependency,
+  ProFormGroup,
+} from '@ant-design/pro-form';
 import type { listFormDataType } from './data';
 
 const BasisForm: React.FC = () => {
@@ -19,57 +24,123 @@ const BasisForm: React.FC = () => {
 
   return (
     <PageContainer>
-      <ProCard>
-        <ProForm
-          submitter={{
-            render: (_, dom) => <FooterToolbar>{dom}</FooterToolbar>,
-          }}
-          onFinish={async (values) => {
-            submitForm(values);
-          }}
-        >
-          <ProFormList
-            name={['default', 'users']}
-            label="用户信息"
-            creatorButtonProps={{
-              creatorButtonText: '再建一行',
-            }}
-            initialValue={[
-              {
-                name: '1111',
-              },
-            ]}
-            itemContainerRender={(doms) => {
-              return <ProForm.Group>{doms}</ProForm.Group>;
+      <ProCard gutter={[{ xs: 8, sm: 8, md: 16, lg: 24, xl: 32 }, 16]}>
+        <ProCard title="简单列表表单" headerBordered bordered>
+          <ProForm
+            onFinish={async (values) => {
+              submitForm(values);
             }}
           >
-            {(f, index, action) => {
-              console.log(f, index, action);
-              return (
-                <>
-                  <ProFormText initialValue={index} name="rowKey" label={`第 ${index} 配置`} />
-                  <ProFormText name="name" label="姓名" />
-                  <ProFormDependency name={['name']}>
-                    {({ name }) => {
-                      if (!name) {
-                        return (
-                          <span
-                            style={{
-                              lineHeight: '32px',
-                            }}
-                          >
-                            输入姓名展示
-                          </span>
-                        );
-                      }
-                      return <ProFormText name="remark" label="昵称详情" />;
-                    }}
-                  </ProFormDependency>
-                </>
-              );
+            <ProFormList
+              name="data"
+              creatorButtonProps={{
+                creatorButtonText: '新加一行数据',
+              }}
+              initialValue={[
+                {
+                  content1: '内容1',
+                  content2: '内容2',
+                },
+              ]}
+              itemContainerRender={(doms) => {
+                return <ProForm.Group>{doms}</ProForm.Group>;
+              }}
+            >
+              {(f, index, action) => {
+                console.log(f, index, action);
+                return (
+                  <Fragment>
+                    <ProFormText name="content1" label="内容1" />
+                    <ProFormText
+                      name="content2"
+                      label="内容2"
+                      required
+                      rules={[{ required: true, message: '该项是必填项!' }]}
+                    />
+                    <ProFormDependency name={['content2']}>
+                      {({ content2 }) => {
+                        if (!content2) {
+                          return (
+                            <span
+                              style={{
+                                lineHeight: '32px',
+                              }}
+                            >
+                              输入内容2展示
+                            </span>
+                          );
+                        }
+                        return <ProFormText name="content3" label="内容3" />;
+                      }}
+                    </ProFormDependency>
+                  </Fragment>
+                );
+              }}
+            </ProFormList>
+          </ProForm>
+        </ProCard>
+
+        <ProCard title="嵌套列表表单" headerBordered bordered>
+          <ProForm
+            onFinish={async (values) => {
+              submitForm(values);
             }}
-          </ProFormList>
-        </ProForm>
+          >
+            <ProFormList
+              name="data"
+              initialValue={[
+                {
+                  name: '标题1',
+                },
+              ]}
+              creatorButtonProps={{
+                creatorButtonText: '新加一页数据',
+              }}
+              itemRender={({ listDom, action }, { record }) => {
+                return (
+                  <ProCard
+                    bordered
+                    extra={action}
+                    title={record?.name}
+                    style={{
+                      marginBottom: 8,
+                    }}
+                  >
+                    {listDom}
+                  </ProCard>
+                );
+              }}
+            >
+              <ProFormGroup>
+                <ProFormText name="name" label="姓名" />
+                <ProFormText name="nickName" label="昵称" />
+              </ProFormGroup>
+              <ProFormList
+                name="labels"
+                initialValue={[
+                  {
+                    value: '333',
+                    label: '333',
+                  },
+                ]}
+                creatorButtonProps={{
+                  creatorButtonText: '新加一行数据',
+                }}
+                copyIconProps={{
+                  tooltipText: '复制此行到末尾',
+                }}
+                deleteIconProps={{
+                  tooltipText: '不需要这行了',
+                }}
+              >
+                <ProFormGroup>
+                  <ProFormText name="value" label="值" />
+                  <ProFormText name="label" label="显示名称" />
+                </ProFormGroup>
+              </ProFormList>
+            </ProFormList>
+          </ProForm>
+        </ProCard>
       </ProCard>
     </PageContainer>
   );
